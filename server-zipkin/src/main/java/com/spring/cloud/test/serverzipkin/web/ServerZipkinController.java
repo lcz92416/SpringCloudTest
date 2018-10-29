a139b90355c9fbc473c061e79d8acf6cdf23d0b9
lichenzhe <lichenzhe@iboxpay.com>
2018-10-29 16:54:12 +0800

    Spring Cloud

---
+++ server-zipkin/src/main/java/com/spring/cloud/test/serverzipkin/web/ServerZipkinController.java
@@ -0,0 +1,40 @@
+package com.spring.cloud.test.serverzipkin.web;
+
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+public class ServerZipkinController {
+    private static final Logger LOG = Logger.getLogger(ServerZipkinController.class.getName());
+    @Autowired
+    private RestTemplate restTemplate;
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+    @RequestMapping("/hi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8756/miya", String.class);
+    }
+    @RequestMapping("/info")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi ");
+
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+}
